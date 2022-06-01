795a3096af49e4938800a3c0f6839d12ce71761a
Xavier <beracoecheaxavier@gmail.com>
2022-06-01 08:34:51 -0300

    added UI

--- app.js
+++ app.js
@@ -1,28 +1,56 @@
-const options = ["rock", "paper", "scissors"];
-// const computerSelect = options[Math.floor(Math.random()*options.length)];
+const options = ["Rock", "Paper", "Scissors"];
 
-for (var i = 0; i < 5; i++) {
-    const computerSelect = options[Math.floor(Math.random()*options.length)];
-    playerSelect = prompt("Please choose between: rock, paper and scissors.");
-    console.log(`Player chose: ${playerSelect}`);
-    console.log(`Computer chose: ${computerSelect}`);
-    playRound(playerSelect, computerSelect);
+let userScr = 0;
+let computerScr = 0;
+
+const buttons = document.querySelectorAll('button');
+let playerDisplay = document.querySelector('.player');
+let computerDisplay = document.querySelector('.computer');
+let resultDisplay = document.querySelector('.result');
+
+let userSelect;
+let computerSelect;
+let result;
+
+buttons.forEach(button => button.addEventListener('click', (e) => {
+    computerSelect = options[Math.floor(Math.random()*options.length)];
+    userSelect = e.target.innerHTML;
+    playerDisplay.innerHTML = userSelect;
+    computerDisplay.innerHTML = computerSelect;
+    playRound();
+    updateScore();
+    if (userScr >= 5) {
+        alert("The user won the game! (●'◡'●)")
+        location.reload();
+    } else if(computerScr >= 5) {
+        alert("The computer won the game （＞人＜；)")
+        location.reload();
+    }
+}));
+
+function playRound() {
+    if (computerSelect === userSelect) {
+        result = "It's a tie!";
+    } if (computerSelect === 'Rock' && userSelect === "Paper" ) {
+        result = "User wins!";
+    } if (computerSelect === "Paper" && userSelect === "Scissors") {
+        result = "User wins!";
+    } if (computerSelect === "Rock" && userSelect === "Scissors") {
+        result = "Computer wins!";
+    } if (computerSelect === "Scissors" && userSelect === "Rock") {
+        result = "User wins!";
+    } if (computerSelect === "Paper" && userSelect === "Rock") {
+        result = "Computer wins!";
+    } if (computerSelect === "Scissors" && userSelect === "Paper") {
+        result = "Computer wins!";
+    } 
+    resultDisplay.innerHTML = result;
 }
 
-function playRound(playerSelect, computerSelect) {
-    if (playerSelect === computerSelect) {
-        console.log("It's a tie.");
-    } else if (playerSelect == 'rock' && computerSelect == "paper" ) {
-        console.log("Computer won, paper beats rock.");
-    } else if (playerSelect == "paper" && computerSelect == "scissors") {
-        console.log("Computer won, scissors beat paper.");
-    } else if (playerSelect == "rock" && computerSelect == "scissors") {
-        console.log("Player won, rock beats scissors.");
-    } else if (playerSelect == "scissors" && computerSelect == "rock") {
-        console.log("Computer won, rock beats scissors.");
-    } else if (playerSelect == "paper" && computerSelect == "rock") {
-        console.log("Player won, paper beats rock.");
-    } else if (playerSelect == "scissors" && computerSelect == "paper") {
-        console.log("Player won, scissors beat paper");
-    }
+function updateScore() {
+    if (result === "User wins!") ++userScr;
+    else if (result === "Computer wins!") ++computerScr;
+
+    const liveScore = document.querySelector(".live-score");
+    liveScore.innerHTML = `${userScr} - ${computerScr}`;
 }
